055fbe00dffffe12ca1196609f336c1ec05d7087
jasminabualrub <jasminabualrub1999@gmail.com>
2024-01-23 01:26:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+async function getElements(){
+    const response=await fetch('https://dummyjson.com/products');
+    const data=await response.json();
+    const products=data.products;
+    //console.log(products);
+    const result= products.map(function(ele){
+return `
+
+<div class='product'>
+<h2>${ele.title}</h2>
+<img src=${ele.thumbnail}>
+<p>${ele.price}$</p>
+<div class='line'></div>
+</div>
+`
+    }).join('');
+    document.querySelector('.product').innerHTML=result;
+}
+
+getElements();
